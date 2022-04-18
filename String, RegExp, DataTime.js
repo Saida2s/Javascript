e799bb3495905603ec81a51016f5072251299c7a
Saida2s <101500963+Saida2s@users.noreply.github.com>
2022-04-18 23:08:08 +0500

    Update String, RegExp, DataTime.js

--- String, RegExp, DataTime.js
+++ String, RegExp, DataTime.js
@@ -33,3 +33,32 @@ alert( date.getMonth()+' Month' );
 alert( date.getFullYear()+' Year' );
 
 /////
+
+
+//...............1...................
+var str = 'ahb acb aeb aeeb adcb axeb'.replace(/a\w\b\s/);;
+console.log(str)
+
+
+let text='ahb acb aeb aeeb adcb axeb'
+console.log(text.slice(0,11))
+
+
+
+
+//...............2....................
+const RegExp=/\d\+\d/
+const str='2+3 223 2223'
+const result=str.match(RegExp)
+
+console.log(result)
+
+//...............3....................
+var day = (new Date()).getDate();
+console.log(day);
+
+var month = (new Date()).getMonth();
+console.log(month);
+
+var year = (new Date()).getFullYear();
+console.log(year);
