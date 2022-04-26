b14f0ed8cf18b28eb2cb85484ea49adeb6502c90
Saida2s <101500963+Saida2s@users.noreply.github.com>
2022-04-26 20:14:52 +0500

    Update Prototype and Inheritance, Classes, Constructor.js

--- Prototype and Inheritance, Classes, Constructor.js
+++ Prototype and Inheritance, Classes, Constructor.js
@@ -1,17 +1,17 @@
-
-//Создать 2 объекта: animal и cat, в объект animal добавить свойство move, объект cat должен наследовать свойство move, реализовать и через прототипы и через классы.
-
-
-const animal= new Object({
-    name: name
-})
-
-Object.prototype.features=function(){
-    console.log('move')
+class Animal {
+    constructor(name) {
+        this.canMove = 0;
+        this.name = name;
+    }
+    run(move) {
+        this.canMove = move;
+        alert(`${this.name} moving with speed ${this.canMove}.`);
+    }
+    sitting() {
+        this.canMove = 0;
+        alert(`${this.name} sitting.`);
+    }
 }
-
-let cat;
-cat = Object.create(animal);
 
 
 class Cat extends Animal {
@@ -20,7 +20,6 @@ class Cat extends Animal {
     }
 }
 
-let cat = new Cat("White cat");
 
 cat.run(5);
 cat.hide();
