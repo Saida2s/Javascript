55980ff102d3445ada2b8932018929a8534e5296
Saida2s <101500963+Saida2s@users.noreply.github.com>
2022-04-27 22:03:43 +0500

    Update String, RegExp, DataTime.js
    in the first task i wrote to expressions. I think two of them correct, but second one is not working.
     Can you text me where  is problem?

--- String, RegExp, DataTime.js
+++ String, RegExp, DataTime.js
@@ -1,31 +1,32 @@
-
-
-/////
-
-
 //...............1...................
-var str = 'ahb acb aeb aeeb adcb axeb'.replace(/a\w\b\s/);;
-console.log(str)
 
 
-let text='ahb acb aeb aeeb adcb axeb'
-console.log(text.slice(0,11))
 
 
+
+var reG = /\w{3}\s/g;
+var strGG = "ahb acb aeb aeebadcbaxeb";
+var myArrayG = strGG.match(reG);
+console.log(myArrayG);
+
+var re = /\a\w\b/g;
+var strG = "ahb acb aeb aeebadcbaxeb";
+var myArray = strG.match(re);
+console.log(myArray);
 
 
 //...............2....................
-const RegExp=/\d\+\d/
-const str='2+3 223 2223'
-const result=str.match(RegExp)
+const Regexp = /\d\+\d/;
+const str = '2+3 223 2223';
+const result = str.match(Regexp);
 
-console.log(result)
+console.log(result);
 
 //...............3....................
 var day = (new Date()).getDate();
 console.log(day);
 
-var month = (new Date()).getMonth();
+var month = ((new Date()).getMonth()+1);
 console.log(month);
 
 var year = (new Date()).getFullYear();
